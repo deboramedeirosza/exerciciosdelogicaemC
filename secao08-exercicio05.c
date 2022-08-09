b6170d5d9f2fb3ef8e27bf6b60e5d65842eb3d9a
Débora Medeiros <103949313+deboramedeirosza@users.noreply.github.com>
2022-08-09 16:59:02 -0300

    Create secao08-exercicio05.c

---
+++ secao08-exercicio05.c
@@ -0,0 +1,26 @@
+/* Faça um programa que carregue um vetor com dez números inteiros. Calcule e mostre os números superiores a 50 e suas respectivas posições.
+Mostrar mensagem se não existir nenhum número nesta condição.
+*/
+
+#include <stdio.h>
+
+int main(){
+	//veriáveis
+	int vetor[10];
+	char tem_maior_50 = 0;
+
+	//entradas
+	for(int i = 0; i < 10; i++){
+		printf("Informe o valor para o vetor %d/10: ", (i+1));
+		scanf("%d", &vetor[i]);
+	}
+	for(int i = 0; i < 10; i++){
+		if(vetor[i] > 50){
+			printf("O número %d está na posição %d\n", vetor[i], i);
+			tem_maior_50 = 1;
+					}
+	}
+	if(tem_maior_50 < 1){
+		printf("Não existe número maior que 50.");
+	}
+}
